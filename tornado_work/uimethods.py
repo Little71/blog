61f2750a8c25cc9142ebcdf50de99e4655d7a3bc
mr7 <596786272@qq.com>
2019-04-23 16:03:58 +0800

    init

--- tornado_work/uimethods.py
+++
@@ -1,7 +0,0 @@
-from tornado import escape
-
-def tag(self):
-    return "<h1>aaa</h1>"
-    # return escape.xhtml_escape("<h1>aaa</h1>")
-    return escape.xhtml_unescape("<h1>aaa</h1>")
-    return 'uimethods'
