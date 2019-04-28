4d0269898874dbfee10b76e2574311a9893f483b
mr7 <596786272@qq.com>
2019-04-28 17:58:37 +0800

    aa

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index',methods=['POST','GET'])
 def index():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    print(username,password)
 
     if username == "root" and password == "123456":
         return jsonify({'state': True,'msg':'登录成功'}),'200 ok'
-    return jsonify({'state':False,'msg':'用户名密码错误'}),'403'
+    return jsonify({'state':False,'msg':'用户名密码错误'}),'404'
+
 
 
 if __name__ == '__main__':
