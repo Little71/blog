c57073b2db1ca46873e553685639a66c1be81918
mr7 <596786272@qq.com>
2019-04-28 10:49:11 +0800

    test work

---
+++ test/day47/tsets.js
@@ -0,0 +1,28 @@
+
+/**
+ * Created by Administrator on 2017/2/14.
+ */
+
+
+
+// alert(666);
+
+var func = function (a,b){
+    console.log(arguments.length)
+    console.log(arguments)
+    return a+b;
+};
+console.log(func(1,2))；
+
+window.alert()；
+
+
+
+
+
+
+
+
+
+
+
