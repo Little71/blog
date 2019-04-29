dd8f854f99069dffe7aa778ea8905291f794dfa9
mr7 <596786272@qq.com>
2019-04-29 17:03:01 +0800

    urls

--- s9day88/app01/stark.py
+++ s9day88/app01/stark.py
@@ -1,24 +1,17 @@
-
-
-from stark.service.stark import site,ModelStark
+from stark.service.stark import site, ModelStark
 
 from django.urls import reverse
 from .models import *
 
-
 from django.utils.safestring import mark_safe
+
 
 class UserConfig(ModelStark):
 
-    pass
+    list_display = ['pk', 'name', 'age']
 
-site.register(UserInfo,UserConfig)
+
+site.register(UserInfo, UserConfig)
 site.register(Book)
 
-
-print("_registry ",site._registry)
-
-
-
-
-
+print("_registry ", site._registry)
--- s9day88/s9day88/urls.py
+++ s9day88/s9day88/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 from stark.service.stark import site
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^stark/', site.urls),
